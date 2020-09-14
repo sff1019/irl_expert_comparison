0210d74275f078395288b8d2447aeb7a5d4e2806
sff1019 <starfield1019@gmail.com>
2020-09-15 00:25:24 +0900

    Add all experiments

---
+++ scripts/ddpg_pendulum.py
@@ -0,0 +1,64 @@
+import torch
+from torch.nn import functional as F
+
+from garage import wrap_experiment
+from garage.envs import GymEnv, normalize
+from garage.experiment.deterministic import set_seed
+from garage.np.exploration_policies import AddOrnsteinUhlenbeckNoise
+from garage.replay_buffer import PathBuffer
+from garage.torch.algos import DDPG
+from garage.torch.policies import DeterministicMLPPolicy
+from garage.torch.q_functions import ContinuousMLPQFunction
+from garage.trainer import Trainer
+
+
+@wrap_experiment(log_dir='data/ddpg_pendulum_v0', snapshot_mode='all')
+def ddpg_pendulum(ctxt=None, seed=0, lr=1e-4):
+    """Train DDPG with InvertedDoublePendulum-v2 environment.
+
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by Trainer to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+        lr (float): Learning rate for policy optimization.
+
+    """
+    set_seed(seed)
+    trainer = Trainer(ctxt)
+    env = normalize(GymEnv('Pendulum-v0'))
+
+    policy = DeterministicMLPPolicy(env_spec=env.spec,
+                                    hidden_sizes=[64, 64],
+                                    hidden_nonlinearity=F.relu,
+                                    output_nonlinearity=torch.tanh)
+
+    exploration_policy = AddOrnsteinUhlenbeckNoise(env.spec, policy, sigma=0.2)
+
+    qf = ContinuousMLPQFunction(env_spec=env.spec,
+                                hidden_sizes=[64, 64],
+                                hidden_nonlinearity=F.relu)
+
+    replay_buffer = PathBuffer(capacity_in_transitions=int(1e6))
+
+    policy_optimizer = (torch.optim.Adagrad, {'lr': lr, 'lr_decay': 0.99})
+
+    ddpg = DDPG(env_spec=env.spec,
+                policy=policy,
+                qf=qf,
+                replay_buffer=replay_buffer,
+                steps_per_epoch=20,
+                n_train_steps=50,
+                min_buffer_size=int(1e4),
+                exploration_policy=exploration_policy,
+                target_update_tau=1e-2,
+                discount=0.9,
+                policy_optimizer=policy_optimizer,
+                qf_optimizer=torch.optim.Adam)
+
+    trainer.setup(algo=ddpg, env=env)
+
+    trainer.train(n_epochs=200, batch_size=100)
+
+
+ddpg_pendulum()
---
+++ scripts/sac_pendulum_batch.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""This is an example to train a task with SAC algorithm written in PyTorch."""
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from garage import wrap_experiment
+from garage.envs import GymEnv, normalize
+from garage.experiment.deterministic import set_seed
+from garage.replay_buffer import PathBuffer
+from garage.sampler import LocalSampler
+from garage.torch import set_gpu_mode
+from garage.torch.algos import SAC
+from garage.torch.policies import TanhGaussianMLPPolicy
+from garage.torch.q_functions import ContinuousMLPQFunction
+from garage.trainer import Trainer
+
+
+@wrap_experiment(log_dir='data/sac_pendulum_v0', snapshot_mode='all')
+def sac_pendulum_batch(ctxt=None, seed=1):
+    """Set up environment and algorithm and run the task.
+
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by Trainer to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+
+    """
+    set_seed(seed)
+    trainer = Trainer(snapshot_config=ctxt)
+    env = normalize(GymEnv('Pendulum-v0'))
+
+    policy = TanhGaussianMLPPolicy(
+        env_spec=env.spec,
+        hidden_sizes=[256, 256],
+        hidden_nonlinearity=nn.ReLU,
+        output_nonlinearity=None,
+        min_std=np.exp(-20.),
+        max_std=np.exp(2.),
+    )
+
+    qf1 = ContinuousMLPQFunction(env_spec=env.spec,
+                                 hidden_sizes=[256, 256],
+                                 hidden_nonlinearity=F.relu)
+
+    qf2 = ContinuousMLPQFunction(env_spec=env.spec,
+                                 hidden_sizes=[256, 256],
+                                 hidden_nonlinearity=F.relu)
+
+    replay_buffer = PathBuffer(capacity_in_transitions=int(1e6))
+
+    sac = SAC(env_spec=env.spec,
+              policy=policy,
+              qf1=qf1,
+              qf2=qf2,
+              gradient_steps_per_itr=1000,
+              max_episode_length_eval=1000,
+              replay_buffer=replay_buffer,
+              min_buffer_size=1e4,
+              target_update_tau=5e-3,
+              discount=0.99,
+              buffer_batch_size=256,
+              reward_scale=1.,
+              steps_per_epoch=1)
+
+    if torch.cuda.is_available():
+        set_gpu_mode(True)
+    else:
+        set_gpu_mode(False)
+    sac.to()
+    trainer.setup(algo=sac, env=env, sampler_cls=LocalSampler)
+    trainer.train(n_epochs=200, batch_size=1000)
+
+
+s = np.random.randint(0, 1000)
+sac_pendulum_batch(seed=0)
